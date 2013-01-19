4984806b2467fbb39695ce6e2f99d59fbec50d47
Otto Yiu <otto@live.ca>
2013-01-19 14:12:26 -0800

    increment version number to v0.02

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='django-cors-headers',
-    version='0.01',
+    version='0.02',
     description='Django app for handling the server headers required for Cross-Origin Resource Sharing (CORS).',
     author='Otto Yiu',
+    author_email='otto@live.ca',
     url='https://github.com/OttoYiu/django-cors-headers',
     packages=['corsheaders'],
 )
