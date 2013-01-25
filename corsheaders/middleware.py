b35252198a240c6d320ba36c678678b98aa14e0c
Otto Yiu <otto@live.ca>
2013-01-25 14:49:47 -0800

    fixed #3 by intercepting CORS preflight requests and creating an empty response for it

--- corsheaders/middleware.py
+++ corsheaders/middleware.py
@@ -1,10 +1,27 @@
+from django import http
 from urlparse import urlparse
 
 from corsheaders import defaults as settings
 
 
 class CorsMiddleware(object):
+
+    def process_request(self, request):
+        '''
+            If CORS preflight header, then create an empty body response (200 OK) and return it
+
+            Django won't bother calling any other request view/exception middleware along with
+            the requested view; it will call any response middlewares
+        '''
+        if request.method == 'OPTIONS' and request.META.get('HTTP_ACCESS_CONTROL_REQUEST_METHOD'):
+            response = http.HttpResponse()
+            return response
+        return None
+
     def process_response(self, request, response):
+        '''
+            Add the respective CORS headers
+        '''
         origin = request.META.get('HTTP_ORIGIN')
         if origin:
             # todo: check hostname from db instead
