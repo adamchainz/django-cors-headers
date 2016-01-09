fd0a6de61aaf90aa102ab450984dcaf66be0a281
Augustin Laville <augustin.laville@gustavi.net>
2016-01-09 22:42:23 +0100

    Django 1.9 support

--- tests.py
+++ tests.py
@@ -8,19 +8,34 @@ def run_tests():
     import django
     from django.conf import global_settings
     from django.conf import settings
-    settings.configure(
-        INSTALLED_APPS=[
-            'corsheaders',
-        ],
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'TEST_NAME': ':memory:',
+    try:
+        settings.configure(
+            INSTALLED_APPS=[
+                'corsheaders',
+            ],
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'TEST_NAME': ':memory:',
+                },
             },
-        },
-        MIDDLEWARE_CLASSES=global_settings.MIDDLEWARE_CLASSES + (
-            'corsheaders.middleware.CorsMiddleware',),
-    )
+            MIDDLEWARE_CLASSES=global_settings.MIDDLEWARE_CLASSES + (
+                'corsheaders.middleware.CorsMiddleware',),
+        )
+    except TypeError:  # django 1.9+
+        settings.configure(
+            INSTALLED_APPS=[
+                'corsheaders',
+            ],
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'TEST_NAME': ':memory:',
+                },
+            },
+            MIDDLEWARE_CLASSES=global_settings.MIDDLEWARE_CLASSES + [
+                'corsheaders.middleware.CorsMiddleware',],
+        )
     if hasattr(django, 'setup'):
         django.setup()
 
