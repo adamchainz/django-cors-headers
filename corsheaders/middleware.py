90fbdace2113fc9dbf6343f4227d39dbaa82912b
Lihan Li <lilihan.it@gmail.com>
2016-06-11 12:11:34 +1000

    Bug fix: If whitelist regex matches the origin, then it should add the origin to ACCESS_CONTROL_ALLOW_ORIGIN header

--- corsheaders/middleware.py
+++ corsheaders/middleware.py
@@ -108,14 +108,17 @@ class CorsMiddleware(object):
             # todo: check hostname from db instead
             url = urlparse(origin)
 
+            if (not settings.CORS_ORIGIN_ALLOW_ALL and
+                    self.origin_not_found_in_white_lists(origin, url)):
+                return response
+
             if settings.CORS_MODEL is not None:
                 model = get_model(*settings.CORS_MODEL.split('.'))
                 if model.objects.filter(cors=url.netloc).exists():
                     response[ACCESS_CONTROL_ALLOW_ORIGIN] = origin
 
-            if (not settings.CORS_ORIGIN_ALLOW_ALL and
-                    self.origin_not_found_in_white_lists(origin, url)):
-                return response
+            elif self.regex_domain_match(origin):
+                response[ACCESS_CONTROL_ALLOW_ORIGIN] = origin
 
             response[ACCESS_CONTROL_ALLOW_ORIGIN] = "*" if (
                 settings.CORS_ORIGIN_ALLOW_ALL and
