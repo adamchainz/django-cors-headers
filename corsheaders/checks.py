88694c0acdf3146839d31ea8b8a36d178cd01196
ittus <vuminhthang.cm@gmail.com>
2016-10-14 11:53:14 +0800

    add test checks for validation settings

--- corsheaders/checks.py
+++ corsheaders/checks.py
@@ -10,79 +10,79 @@ def check_settings(app_configs, **kwargs):
     errors = []
     if not isinstance(corsheaders_settings.CORS_ALLOW_HEADERS, collections.Sequence):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_ALLOW_HEADERS must be sequence",
-                id="django-cors-headers.W001"
+                id="corsheaders.W001"
             )
         )
     if not isinstance(corsheaders_settings.CORS_ALLOW_METHODS, collections.Sequence):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_ALLOW_METHODS must be sequence",
-                id="django-cors-headers.W002"
+                id="corsheaders.W002"
             )
         )
     if not isinstance(corsheaders_settings.CORS_ALLOW_CREDENTIALS, bool):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_ALLOW_CREDENTIALS must be bool",
-                id="django-cors-headers.W003"
+                id="corsheaders.W003"
             )
         )
     if not isinstance(corsheaders_settings.CORS_PREFLIGHT_MAX_AGE, numbers.Integral):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_PREFLIGHT_MAX_AGE must be integer",
-                id = "django-cors-headers.W004"
+                id = "corsheaders.W004"
             )
         )
     if not isinstance(corsheaders_settings.CORS_ORIGIN_ALLOW_ALL, bool):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_ORIGIN_ALLOW_ALL must be bool",
-                id="django-cors-headers.W005"
+                id="corsheaders.W005"
             )
         )
     if not isinstance(corsheaders_settings.CORS_ORIGIN_WHITELIST, collections.Sequence):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_ORIGIN_WHITELIST must be sequence",
-                id="django-cors-headers.W006"
+                id="corsheaders.W006"
             )
         )
     if not isinstance(corsheaders_settings.CORS_ORIGIN_REGEX_WHITELIST, collections.Sequence):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_ORIGIN_REGEX_WHITELIST must be sequence",
-                id="django-cors-headers.W007"
+                id="corsheaders.W007"
             )
         )
     if not isinstance(corsheaders_settings.CORS_EXPOSE_HEADERS, collections.Sequence):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_EXPOSE_HEADERS must be sequence",
-                id="django-cors-headers.W008"
+                id="corsheaders.W008"
             )
         )
     if not isinstance(corsheaders_settings.CORS_URLS_REGEX, str):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_URLS_REGEX must be regex string",
-                id="django-cors-headers.W009"
+                id="corsheaders.W009"
             )
         )
-    if corsheaders_settings.CORS_MODEL and not isinstance(corsheaders_settings.CORS_MODEL, str):
+    if corsheaders_settings.CORS_MODEL is not None and not isinstance(corsheaders_settings.CORS_MODEL, str):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_MODEL must be string",
-                id="django-cors-headers.W0010"
+                id="corsheaders.W0010"
             )
         )
     if not isinstance(corsheaders_settings.CORS_REPLACE_HTTPS_REFERER, bool):
         errors.append(
-            checks.Warning(
+            checks.Error(
                 "CORS_REPLACE_HTTPS_REFERER must be bool",
-                id="django-cors-headers.W0011"
+                id="corsheaders.W0011"
             )
         )
     return errors
---
+++ tests/test_checks.py
@@ -0,0 +1,142 @@
+from django.test import SimpleTestCase
+from django.test.utils import override_settings
+from django.core import checks
+
+from corsheaders.conf import corsheaders_settings
+from corsheaders.checks import check_settings
+
+from corsheaders.defaults import default_headers, default_methods
+
+class SettingsTests(SimpleTestCase):
+
+    @override_settings(CORS_ALLOW_HEADERS=True)
+    def test_cors_allow_headers_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W001"
+
+    @override_settings(CORS_ALLOW_METHODS=1)
+    def test_cors_allow_methods_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W002"
+
+    @override_settings(CORS_ALLOW_CREDENTIALS=())
+    def test_cors_allow_credentials_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W003"
+
+    @override_settings(CORS_PREFLIGHT_MAX_AGE="10")
+    def test_cors_preflight_max_age_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W004"
+
+    @override_settings(CORS_ORIGIN_ALLOW_ALL=0)
+    def test_cors_origin_allow_all_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W005"
+
+    @override_settings(CORS_ORIGIN_WHITELIST=True)
+    def test_cors_origin_whitelist_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W006"
+
+    @override_settings(CORS_ORIGIN_REGEX_WHITELIST=10)
+    def test_cors_origin_regex_whitelist_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W007"
+
+    @override_settings(CORS_EXPOSE_HEADERS=True)
+    def test_cors_expose_headers_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W008"
+
+    @override_settings(CORS_URLS_REGEX=())
+    def test_cors_urls_regex_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W009"
+
+    @override_settings(CORS_MODEL=())
+    def test_cors_model_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W0010"
+
+    @override_settings(CORS_REPLACE_HTTPS_REFERER=())
+    def test_cors_replace_https_referer_failure(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) > 0
+        assert isinstance(errors[0], checks.Error)
+        assert errors[0].id == "corsheaders.W0011"
+
+    @override_settings(CORS_ALLOW_HEADERS=default_headers)
+    def test_cors_allow_headers_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_ALLOW_METHODS=default_methods)
+    def test_cors_allow_methods_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_ALLOW_CREDENTIALS=True)
+    def test_cors_allow_credentials_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_PREFLIGHT_MAX_AGE=86400)
+    def test_cors_preflight_max_age_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_ORIGIN_ALLOW_ALL=True)
+    def test_cors_origin_allow_all_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_ORIGIN_WHITELIST=())
+    def test_cors_origin_whitelist_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_ORIGIN_REGEX_WHITELIST=())
+    def test_cors_origin_regex_whitelist_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_EXPOSE_HEADERS=())
+    def test_cors_expose_headers_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_URLS_REGEX=r'^.*$')
+    def test_cors_urls_regex_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_MODEL=None)
+    def test_cors_model_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
+
+    @override_settings(CORS_REPLACE_HTTPS_REFERER=True)
+    def test_cors_replace_https_referer_success(self):
+        errors = check_settings(corsheaders_settings)
+        assert len(errors) == 0
