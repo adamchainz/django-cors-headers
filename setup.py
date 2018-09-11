9cdbbb53783935b1ab6075b7efad14dc8d046d81
Michael <michael-k@users.noreply.github.com>
2018-09-11 20:37:29 +0000

    Document support for Django 2.1 (#341)

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
