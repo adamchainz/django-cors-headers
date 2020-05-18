2b275fd20c6ef8f1c370ca4b00e03d515b88b98c
Matthias Kestenholz <mk@feinheit.ch>
2020-05-18 16:18:49 +0200

    Remove the deprecated `providing_args` from Signal (#516)
    See also https://code.djangoproject.com/ticket/31327

--- src/corsheaders/signals.py
+++ src/corsheaders/signals.py
@@ -3,4 +3,4 @@ from django.dispatch import Signal
 # If any attached handler returns Truthy, CORS will be allowed for the request.
 # This can be used to build custom logic into the request handling when the
 # configuration doesn't work.
-check_request_enabled = Signal(providing_args=["request"])
+check_request_enabled = Signal()
