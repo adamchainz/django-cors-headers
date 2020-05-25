bcbc9457a865b507465ae22affcd4065094f99d0
Adam Johnson <me@adamj.eu>
2020-05-25 17:53:09 +0100

    Test with Python 3.9

--- requirements/compile.py
+++ requirements/compile.py
@@ -185,3 +185,36 @@ if __name__ == "__main__":
         ],
         check=True,
     )
+    subprocess.run(
+        [
+            "python3.9",
+            *common_args,
+            "-P",
+            "Django>=2.2,<2.3",
+            "-o",
+            "py39-django22.txt",
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "python3.9",
+            *common_args,
+            "-P",
+            "Django>=3.0a1,<3.1",
+            "-o",
+            "py39-django30.txt",
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "python3.9",
+            *common_args,
+            "-P",
+            "Django>=3.1a1,<3.2",
+            "-o",
+            "py39-django31.txt",
+        ],
+        check=True,
+    )
