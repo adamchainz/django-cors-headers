42b898a459d56f420939534a452b38915c06a6b8
Adam Johnson <me@adamj.eu>
2024-06-19 16:43:53 +0100

    Support Django 5.1 (#953)

--- requirements/compile.py
+++ requirements/compile.py
@@ -155,6 +155,16 @@ if __name__ == "__main__":
         [
             *common_args,
             "--python",
+            "3.10",
+            "--output-file",
+            "py310-django51.txt",
+        ],
+        input=b"Django>=5.1a1,<5.2",
+    )
+    run(
+        [
+            *common_args,
+            "--python",
             "3.11",
             "--output-file",
             "py311-django41.txt",
@@ -185,6 +195,16 @@ if __name__ == "__main__":
         [
             *common_args,
             "--python",
+            "3.11",
+            "--output-file",
+            "py311-django51.txt",
+        ],
+        input=b"Django>=5.1a1,<5.2",
+    )
+    run(
+        [
+            *common_args,
+            "--python",
             "3.12",
             "--output-file",
             "py312-django42.txt",
@@ -200,4 +220,14 @@ if __name__ == "__main__":
             "py312-django50.txt",
         ],
         input=b"Django>=5.0a1,<5.1",
+    )
+    run(
+        [
+            *common_args,
+            "--python",
+            "3.12",
+            "--output-file",
+            "py312-django51.txt",
+        ],
+        input=b"Django>=5.1a1,<5.2",
     )
